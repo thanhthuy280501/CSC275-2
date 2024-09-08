9a40cf8c41dd6cba9f5a456f8ecf8fc2b13f5863
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-09-07 23:18:14 -0400

    TN ic02-1.cpp

--- ic02-1.cpp
+++ ic02-1.cpp
@@ -5,25 +5,98 @@ using namespace std;
 
 //*******************************************************************
 // Program:	in-class exercise #2
-// Author:	(you)
+// Author:	Thuy Nguyen
+// Class: CSC 275
 // Date:	09/06/22 and 09/08/22
 // This program creates instances of class Student and demonstrates
 // the use of various features of the class
 //*******************************************************************
-int main( )
-{
-  Student s1;
-  s1.input();
 
-  s1.output();
+class Student {
+public:
+    // Default constructor
+    Student() : lname("Doe"), fname("John"), ID("00000000"), gpa(0.0) {}
 
-// The next line is an example of usage of a mutator function that could
-// be added, which would allow you to modify just one data member instead
-// of the whole class instance.
+    // Constructor with parameters
+    Student(const string &ln, const string &fn, const string &id, float g) 
+        : lname(ln), fname(fn), ID(id), gpa(g) {}
 
-//  s1.new_lname("chamberlain");
-//  s1.output();
+    // Accessor functions
+    string getLastName() const { return lname; }
+    string getFirstName() const { return fname; }
+    string getID() const { return ID; }
+    float getGPA() const { return gpa; }
 
-  return 0;
+    // Mutator functions
+    void setLastName(const string &LN) { lname = LN; }
+    void setFirstName(const string &FN) { fname = FN; }
+    void setID(const string &Id) { ID = Id; }
+    void setGPA(float GPA) { gpa = GPA; }
+
+    // Inline mutator function
+    void setFullName(const string &LN, const string &FN) {
+        lname = LN;
+        fname = FN;
+    }
+
+    // Non-inline mutator function
+    void setStudentInfo(const string &LN, const string &FN, const string &Id, float GPA) {
+        lname = LN;
+        fname = FN;
+        ID = Id;
+        gpa = GPA;
+    }
+
+    void input(); // Read a student record in using cin.
+    void output(); // Print out the student record, slightly formatted.
+
+private:
+    string lname; // Last name
+    string fname; // First name
+    string ID; // ID, presumably 8-digit but no error checking
+    float gpa; // GPA
+};
+
+// Implementation of input and output functions
+void Student::input() {
+    cout << "Last name: ";
+    cin >> lname;
+    cout << "First name: ";
+    cin >> fname;
+    cout << "ID: ";
+    cin >> ID;
+    cout << "GPA: ";
+    cin >> gpa;
 }
 
+void Student::output() {
+    cout << fname << " " << lname << ", ID " << ID;
+    cout << " has a GPA of " << gpa << endl;
+}
+
+int main( ){
+  Student s1; // (1) Default constructor
+    s1.output();
+
+    Student s2("Smith", "Alice", "12345678", 3.5); // (2) Constructor with parameters
+    s2.output();
+
+    // (4) Mutator functions example
+    s1.setLastName("Doe");
+    s1.setFirstName("Jane");
+    s1.setID("87654321");
+    s1.setGPA(3.0);
+
+    s1.output();
+
+    // (5) Inline mutator function example
+    s1.setFullName("Johnson", "Robert");
+    s1.output();
+
+    // (6) Non-inline mutator function example
+    s1.setStudentInfo("Brown", "Emily", "23456789", 3.7);
+    s1.output();
+
+    return 0;
+}
+ 
